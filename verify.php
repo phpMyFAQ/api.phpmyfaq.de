557b4e6e271852eea39d54fa438c52c5cb7ec8c5
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-04-12 21:53:32 +0200

    Fixed header()

--- verify.php
+++ verify.php
@@ -8,5 +8,6 @@ $version = filter_input(INPUT_GET, 'version', FILTER_SANITIZE_STRIPPED);
 if (preg_match('((\d+)\.(\d+)(\.\d+)?(-(beta|alpha|rc)(\d+))?)', $version) && file_exists('hashes-' . $version . '.json')) {
     print file_get_contents('hashes-' . $version . '.json');
 } else {
+    header("HTTP/1.0 418 I'm a teapot");
     print json_encode('The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge.');
 }
