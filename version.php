1ff94207a93f7f49c9ac0dd8ddd6841e747c5a07
Thorsten Rinne <thorsten@phpmyfaq.de>
2012-04-13 09:48:59 +0200

    Changed key

--- version.php
+++ version.php
@@ -5,7 +5,7 @@ require_once '../version.php';
 
 print json_encode(
     array(
-        'stable'   => PHPMYFAQ_STABLE_VERSION,
-        'unstable' => PHPMYFAQ_DEV_VERSION
+        'stable'      => PHPMYFAQ_STABLE_VERSION,
+        'development' => PHPMYFAQ_DEV_VERSION
     )
 );
