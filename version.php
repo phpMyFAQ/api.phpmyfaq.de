f94ab4ec885421e3e2b5056fd9c041ae05d7a5b5
Thorsten Rinne <thorsten@phpmyfaq.de>
2014-12-24 11:10:35 +0100

    Added URLs for stable and dev versions

--- version.php
+++ version.php
@@ -10,9 +10,26 @@ header('Content-type: application/json');
 
 require 'phpmyfaq.php';
 
-echo json_encode(
-    array(
-        'stable'      => PHPMYFAQ_STABLE_VERSION,
-        'development' => PHPMYFAQ_DEV_VERSION
-    )
-);
+$branch = filter_input(INPUT_GET, 'branch', FILTER_SANITIZE_STRING);
+
+switch ($branch) {
+
+    case 'stable':
+        echo json_encode(PHPMYFAQ_STABLE_VERSION);
+        break;
+
+    case 'development':
+        echo json_encode(PHPMYFAQ_DEV_VERSION);
+        break;
+
+    default:
+        echo json_encode(
+            [
+                'stable'      => PHPMYFAQ_STABLE_VERSION,
+                'development' => PHPMYFAQ_DEV_VERSION
+            ]
+        );
+        break;
+}
+
+exit();
