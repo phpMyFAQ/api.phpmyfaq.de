3c44ba224663a199401ce02b77121f5219457aff
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-02-01 12:59:02 +0100

    2.9.0-alpha2 release

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * phpMyFAQ versions
- *
+ * phpMyFAQ versions + release dates
  *
  */
 
 define('PHPMYFAQ_STABLE_VERSION', '2.8.19');
 define('PHPMYFAQ_STABLE_RELEASE', '2014-12-31');
-define('PHPMYFAQ_DEV_VERSION',    '2.9.0-alpha');
-define('PHPMYFAQ_DEV_RELEASE',    '2014-08-12');
+define('PHPMYFAQ_DEV_VERSION',    '2.9.0-alpha2');
+define('PHPMYFAQ_DEV_RELEASE',    '2015-02-01');
