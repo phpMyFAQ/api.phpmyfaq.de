c67c83925f2d8136a3bf05ffb5e2ed1a87699780
Thorsten Rinne <thorsten@phpmyfaq.de>
2015-07-27 20:12:06 +0200

    2.8.24

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -4,7 +4,7 @@
  *
  */
 
-define('PHPMYFAQ_STABLE_VERSION', '2.8.23');
-define('PHPMYFAQ_STABLE_RELEASE', '2015-06-13');
+define('PHPMYFAQ_STABLE_VERSION', '2.8.24');
+define('PHPMYFAQ_STABLE_RELEASE', '2015-07-27');
 define('PHPMYFAQ_DEV_VERSION',    '2.9.0-alpha2');
 define('PHPMYFAQ_DEV_RELEASE',    '2015-02-01');
