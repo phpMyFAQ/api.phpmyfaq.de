81d4f89693f1cc0574ada204917d16c428b3c924
Thorsten Rinne <thorsten@phpmyfaq.de>
2017-10-19 20:00:23 +0200

    2.9.9

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -4,7 +4,7 @@
  *
  */
 
-define('PHPMYFAQ_STABLE_VERSION', '2.9.8');
-define('PHPMYFAQ_STABLE_RELEASE', '2017-07-12');
+define('PHPMYFAQ_STABLE_VERSION', '2.9.9');
+define('PHPMYFAQ_STABLE_RELEASE', '2017-10-18');
 define('PHPMYFAQ_DEV_VERSION',    '2.10.0-alpha');
 define('PHPMYFAQ_DEV_RELEASE',    '2017-07-19');
