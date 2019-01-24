72a43610f42061a67f04a077a008b0391366dff5
Thorsten Rinne <thorsten@phpmyfaq.de>
2019-01-24 20:13:09 +0100

    3.0.0-alpha.4

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -6,5 +6,5 @@
 
 define('PHPMYFAQ_STABLE_VERSION', '2.9.11');
 define('PHPMYFAQ_STABLE_RELEASE', '2018-09-02');
-define('PHPMYFAQ_DEV_VERSION',    '3.0.0-alpha.3');
-define('PHPMYFAQ_DEV_RELEASE',    '2018-12-21');
+define('PHPMYFAQ_DEV_VERSION',    '3.0.0-alpha.4');
+define('PHPMYFAQ_DEV_RELEASE',    '2019-01-24');
