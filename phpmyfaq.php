44e79546548fd1cdcacd53a10e97b7f626f3853c
Thorsten Rinne <thorsten@phpmyfaq.de>
2019-02-11 19:45:25 +0100

    2.9.12

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -4,7 +4,7 @@
  *
  */
 
-define('PHPMYFAQ_STABLE_VERSION', '2.9.11');
-define('PHPMYFAQ_STABLE_RELEASE', '2018-09-02');
-define('PHPMYFAQ_DEV_VERSION',    '3.0.0-alpha.4');
-define('PHPMYFAQ_DEV_RELEASE',    '2019-01-24');
+define('PHPMYFAQ_STABLE_VERSION', '2.9.12');
+define('PHPMYFAQ_STABLE_RELEASE', '2019-02-12');
+define('PHPMYFAQ_DEV_VERSION',    '3.0.0-beta');
+define('PHPMYFAQ_DEV_RELEASE',    '2019-02-12');
