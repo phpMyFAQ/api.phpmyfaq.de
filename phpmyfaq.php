f222630209ba13d2d94319e00d55a0d50be23090
Thorsten Rinne <thorsten@phpmyfaq.de>
2019-11-30 13:46:28 +0100

    3.0.0-beta.3

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -6,5 +6,5 @@
 
 define('PHPMYFAQ_STABLE_VERSION', '2.9.13');
 define('PHPMYFAQ_STABLE_RELEASE', '2019-02-14');
-define('PHPMYFAQ_DEV_VERSION',    '3.0.0-beta.2');
-define('PHPMYFAQ_DEV_RELEASE',    '2019-02-14');
+define('PHPMYFAQ_DEV_VERSION',    '3.0.0-beta.3');
+define('PHPMYFAQ_DEV_RELEASE',    '2019-11-30');
