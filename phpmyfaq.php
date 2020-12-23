496b09e42f01406a76b51361a821f36e8dc3950e
Thorsten Rinne <thorsten@phpmyfaq.de>
2020-12-23 10:17:25 +0100

    3.0.7 + 3.1.0-alpha.3

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -1,7 +1,7 @@
 <?php
 /** phpMyFAQ versions + release dates */
 
-define('PHPMYFAQ_STABLE_VERSION', '3.0.6');
-define('PHPMYFAQ_STABLE_RELEASE', '2020-11-27');
-define('PHPMYFAQ_DEV_VERSION',    '3.1.0-alpha.2');
-define('PHPMYFAQ_DEV_RELEASE',    '2020-11-15');
+define('PHPMYFAQ_STABLE_VERSION', '3.0.7');
+define('PHPMYFAQ_STABLE_RELEASE', '2020-12-23');
+define('PHPMYFAQ_DEV_VERSION',    '3.1.0-alpha.3');
+define('PHPMYFAQ_DEV_RELEASE',    '2020-12-23');
