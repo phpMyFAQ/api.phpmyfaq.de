5714c5db54fa1f3ddb8f0bfcf0f4612dfc2805c7
Thorsten Rinne <thorsten@phpmyfaq.de>
2021-03-12 20:44:26 +0100

    fixed release date

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -4,4 +4,4 @@
 define('PHPMYFAQ_STABLE_VERSION', '3.0.8');
 define('PHPMYFAQ_STABLE_RELEASE', '2021-02-24');
 define('PHPMYFAQ_DEV_VERSION',    '3.1.0-beta');
-define('PHPMYFAQ_DEV_RELEASE',    '2021-02-12');
+define('PHPMYFAQ_DEV_RELEASE',    '2021-03-12');
