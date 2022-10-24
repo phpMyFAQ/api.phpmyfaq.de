06724ff2f9ba32b22092b68b163f9989b87fd18c
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-10-24 17:58:50 +0200

    3.1.8

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -1,7 +1,7 @@
 <?php
 /** phpMyFAQ versions + release dates */
 
-const PHPMYFAQ_STABLE_VERSION = '3.1.7';
-const PHPMYFAQ_STABLE_RELEASE = '2022-10-02';
+const PHPMYFAQ_STABLE_VERSION = '3.1.8';
+const PHPMYFAQ_STABLE_RELEASE = '2022-10-24';
 const PHPMYFAQ_DEV_VERSION = '3.1.0-RC';
 const PHPMYFAQ_DEV_RELEASE = '2022-01-22';
