13175f06f8a0c5c9208841bd1adc8c0aeb9f9a42
Thorsten Rinne <thorsten@phpmyfaq.de>
2022-12-11 18:32:45 +0100

    refactor: now using PHP8

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * API endpoint for api.phpmyfaq.de/verfiy/<version>
+ * API endpoint for api.phpmyfaq.de/verify/<version>
  *
  *
  */
@@ -10,10 +10,10 @@ header('Content-type: application/json');
 
 require 'phpmyfaq.php';
 
-$version  = filter_input(INPUT_GET, 'version', FILTER_SANITIZE_STRIPPED);
+$version  = filter_input(INPUT_GET, 'version', FILTER_SANITIZE_ADD_SLASHES);
 $fileName = 'json/hashes-' . $version . '.json';
 
-if (preg_match('((\d+)\.(\d+)(\.\d+)?(-(beta|alpha|rc)(\d+))?)', $version) && file_exists($fileName)) {
+if ($version && preg_match('((\d+)\.(\d+)(\.\d+)?(-(beta|alpha|rc)(\d+))?)', $version) && file_exists($fileName)) {
     echo file_get_contents($fileName);
 } else {
     header("HTTP/1.0 418 I'm a teapot");
--- version.php
+++ version.php
@@ -13,28 +13,19 @@ header('Content-type: application/json');
 
 require 'phpmyfaq.php';
 
-$branch = filter_input(INPUT_GET, 'branch', FILTER_SANITIZE_STRING);
+$branch = filter_input(INPUT_GET, 'branch', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-switch ($branch) {
-
-    case 'stable':
-        echo json_encode(PHPMYFAQ_STABLE_VERSION);
-        break;
-
-    case 'development':
-        echo json_encode(PHPMYFAQ_DEV_VERSION);
-        break;
-
-    default:
-        echo json_encode(
-            [
-                'stable'               => PHPMYFAQ_STABLE_VERSION,
-                'stable_released'      => PHPMYFAQ_STABLE_RELEASE,
-                'development'          => PHPMYFAQ_DEV_VERSION,
-                'development_released' => PHPMYFAQ_DEV_RELEASE
-            ]
-        );
-        break;
-}
+echo match ($branch) {
+    'stable' => json_encode(PHPMYFAQ_STABLE_VERSION),
+    'development' => json_encode(PHPMYFAQ_DEV_VERSION),
+    default => json_encode(
+        [
+            'stable' => PHPMYFAQ_STABLE_VERSION,
+            'stable_released' => PHPMYFAQ_STABLE_RELEASE,
+            'development' => PHPMYFAQ_DEV_VERSION,
+            'development_released' => PHPMYFAQ_DEV_RELEASE
+        ]
+    ),
+};
 
 exit();
