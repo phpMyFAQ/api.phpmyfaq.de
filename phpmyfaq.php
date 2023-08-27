9f730027e7763c39308246ec4afb8689c3ce0e73
Thorsten Rinne <thorsten@phpmyfaq.de>
2023-08-27 18:41:51 +0200

    3.1.17

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -1,7 +1,7 @@
 <?php
 /** phpMyFAQ versions + release dates */
 
-const PHPMYFAQ_STABLE_VERSION = '3.1.16';
-const PHPMYFAQ_STABLE_RELEASE = '2023-07-16';
+const PHPMYFAQ_STABLE_VERSION = '3.1.17';
+const PHPMYFAQ_STABLE_RELEASE = '2023-08-27';
 const PHPMYFAQ_DEV_VERSION = '3.2.0-RC.3';
 const PHPMYFAQ_DEV_RELEASE = '2023-07-21';
