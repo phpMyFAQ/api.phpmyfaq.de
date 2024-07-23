895bde77df6831157bac059380da9b32209bdf6a
Thorsten Rinne <thorsten@phpmyfaq.de>
2024-07-23 20:08:16 +0200

    3.2.9 and 4.0.0-alpha.4

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -1,7 +1,7 @@
 <?php
 /** phpMyFAQ versions + release dates */
 
-const PHPMYFAQ_STABLE_VERSION = '3.2.8';
-const PHPMYFAQ_STABLE_RELEASE = '2024-07-21';
-const PHPMYFAQ_DEV_VERSION = '4.0.0-alpha.3';
-const PHPMYFAQ_DEV_RELEASE = '2024-07-21';
+const PHPMYFAQ_STABLE_VERSION = '3.2.9';
+const PHPMYFAQ_STABLE_RELEASE = '2024-07-23';
+const PHPMYFAQ_DEV_VERSION = '4.0.0-alpha.4';
+const PHPMYFAQ_DEV_RELEASE = '2024-07-23';
