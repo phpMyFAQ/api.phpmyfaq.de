4f36b5dfd7a47cd1d08e65ad8467f1f833ba7f22
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-07-06 10:26:26 +0200

    fix: date was wrong

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -2,6 +2,6 @@
 /** phpMyFAQ versions + release dates */
 
 const PHPMYFAQ_STABLE_VERSION = '4.0.9';
-const PHPMYFAQ_STABLE_RELEASE = '2025-07-09';
+const PHPMYFAQ_STABLE_RELEASE = '2025-07-06';
 const PHPMYFAQ_DEV_VERSION = '4.1.0-alpha.2';
 const PHPMYFAQ_DEV_RELEASE = '2025-06-01';
