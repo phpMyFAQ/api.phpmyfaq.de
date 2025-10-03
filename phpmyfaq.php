581ce6e7b87b4151627bb725efc62d7840f84f3a
Thorsten Rinne <thorsten@phpmyfaq.de>
2025-10-03 14:16:25 +0200

    4.0.13

--- phpmyfaq.php
+++ phpmyfaq.php
@@ -1,7 +1,7 @@
 <?php
 /** phpMyFAQ versions + release dates */
 
-const PHPMYFAQ_STABLE_VERSION = '4.0.12';
-const PHPMYFAQ_STABLE_RELEASE = '2025-09-23';
+const PHPMYFAQ_STABLE_VERSION = '4.0.13';
+const PHPMYFAQ_STABLE_RELEASE = '2025-10-03';
 const PHPMYFAQ_DEV_VERSION = '4.1.0-alpha.2';
 const PHPMYFAQ_DEV_RELEASE = '2025-06-01';
